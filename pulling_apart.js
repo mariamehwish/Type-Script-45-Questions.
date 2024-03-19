b4ca20b9a99949f0dce7380d8a8f6979e66dfb2f
Maria Mehwish <108116561+mariamehwish@users.noreply.github.com>
2024-03-19 14:26:19 +0500

    Add files via upload

---
+++ pulling_apart.js
@@ -0,0 +1,8 @@
+"use strict";
+let developerskills = {
+    languages: ["Type Script", "Python", "Java Script"],
+    frameworks: ["Angular", "Vue", "React"],
+    tools: ["Webpack", "Dockar", "Git"]
+};
+let { languages, frameworks, tools } = developerskills;
+console.log(`Languange: ${languages[0]}, Framework: ${frameworks[0]}, tools:${tools[0]}`);
